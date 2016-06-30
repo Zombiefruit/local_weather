fb2f29006ae8bea155998be72fb46e8eaadd08aa
Kieran Williams <kieranwilliams@MacBook-Pro.local>
2016-06-30 13:56:50 +0300

    Pretty much done. Video files are being streamed from google drive (better ways to do this), but it works. Ideally I'd want a loading screen... will revisit when I've finished some other projects.

--- src/resources/js/main.js
+++ src/resources/js/main.js
@@ -1,13 +1,14 @@
 var temp = undefined;
 var state = 0;
+var buttons = "<div id='buttons'><button id='celsius' onclick='changeTemp(0)'>&degC</button><button id='fahr' onclick='changeTemp(1)'>&degF</button></div>";
 
 window.onload = function() {
   var startPos;
-  var geoSuccess = function( position ) {
+  var geoSuccess = function(position) {
     startPos = position;
-    getWeather( startPos );
+    getWeather(startPos);
   };
-  navigator.geolocation.getCurrentPosition( geoSuccess );
+  navigator.geolocation.getCurrentPosition(geoSuccess);
 };
 
 function getWeather(position) {
@@ -29,43 +30,53 @@ function getWeather(position) {
       var tempDisplay = "<p id='temp'>" + temp + "&deg Celsius" + "</p>";
       var humidity = "<p id='humidity'>" + "Humidity: " + weatherObject.main.humidity + "%" + "</p>";
       var weatherType = weatherObject.weather[0].main;
-      weatherType = 'Clear';
 
       var tieBreak = Math.round(Math.random());
       switch (weatherType) {
         case 'Clouds':
           if (tieBreak === 0) {
-            addToElement('bgvid', '<source src="resources/videos/blueSkyClouds.mp4" type="video/mp4">');
+            addToElement('bgvid', '<source src="https://drive.google.com/uc?export=download&id=0BwZgPoiGE7iCWDhiaEZlTFFpVDg" type="video/mp4">');
           } else {
-            addToElement('bgvid', '<source src="resources/videos/cloudsOverField.mp4" type="video/mp4">');
+            addToElement('bgvid', '<source src="https://drive.google.com/uc?export=download&id=0BwZgPoiGE7iCOEhkSnFraWJKNE0" type="video/mp4">');
           }
           break;
         case 'Drizzle':
-          addToElement('bgvid', '<source src="resources/videos/lightRain.mp4" type="video/mp4">');
+          addToElement('bgvid', '<source src="https://drive.google.com/uc?export=download&id=0BwZgPoiGE7iCZ3N2YmREVTlFcmc" type="video/mp4">');
           break;
         case 'Thunderstorm':
         case 'Rain':
-          addToElement('bgvid', '<source src="resources/videos/heavyRain.mp4" type="video/mp4">');
+          addToElement('bgvid', '<source src="https://drive.google.com/uc?export=download&id=0BwZgPoiGE7iCODBLRHFiMnpCZTA" type="video/mp4">');
           break;
         case 'Snow':
-          addToElement('bgvid', '<source src="resources/videos/lightSnow.mp4" type="video/mp4">');
+          addToElement('bgvid', '<source src="https://drive.google.com/uc?export=download&id=0BwZgPoiGE7iCOUxPMVR0MzJxVWc" type="video/mp4">');
           break;
         case 'Clear':
           if (tieBreak === 0) {
-            addToElement('bgvid', '<source src="resources/videos/sunsetGrass.mp4" type="video/mp4">');
+            addToElement('bgvid', '<source src="https://drive.google.com/uc?export=download&id=0BwZgPoiGE7iCalJnakxLODg5UjQ" type="video/mp4">');
           } else {
-            addToElement('bgvid', '<source src="resources/videos/sunny.mp4" type="video/mp4">');
+            addToElement('bgvid', '<source src="https://drive.google.com/uc?export=download&id=0BwZgPoiGE7iCSEhYZUxHeGFhTjA">');
           }
           break;
         default:
-          addToElement('bgvid', '<source src="resources/videos/sunsetClouds.mp4" type="video/mp4">');
+          addToElement('bgvid', '<source src="https://drive.google.com/uc?export=download&id=0BwZgPoiGE7iCcEJSdFpKZFQ5aUU" type="video/mp4">');
       }
 
-      addToElement( 'weather', city, tempDisplay, humidity );
+      addToElement( 'weather', city, tempDisplay, humidity, buttons );
+
+      // This isn't working right now, but optimally, I would like to implement a loading screen that
+      // spins up before the video is loaded so that the user isn't sitting there waiting for the grey
+      // screen to change
+      
+      // var video = document.getElementById('bgvid');
+      // if (video.readyState === 4) {
+      //     console.log("loaded");
+      // } else {
+      //   console.log("video not loaded");
+      // }
     }
   };
 
-  // YOU WOULD NEVER WANT TO EXPOSE AN API KEY IN A "REAL" APP. I'm running this on my local machine and
+  // You wouldn't want to expose the key in a real app. I'm running this on my local machine and
   // it's a free key so I'm taking the liberty to do it this way.
   var key = "af1515bf3429c1c7bf62fc74f5c2066f";
   var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + key;
@@ -75,7 +86,6 @@ function getWeather(position) {
 };
 
 function addToElement(id, string) {
-  document.getElementById(id).innerHTML = "";
   for (i = 1; i < arguments.length; i++) {
     document.getElementById(id).innerHTML += arguments[i];
   }
